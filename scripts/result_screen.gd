b2241097304929e015561e859b6e4094730a1729
mlotr <matteo.lotriglia@icloud.com>
2026-01-14 10:43:13 +0000

    Added result screen sounds

--- scripts/result_screen.gd
+++ scripts/result_screen.gd
@@ -1,7 +1,7 @@
 extends Control
 
 # --- EXPORT VARIABLES ---
-# Trascina qui la scena del Menu Principale (TitleScreen.tscn)
+# Main Menu scene (TitleScreen.tscn)
 @export var title_screen_scene: String = "res://scenes/title_screen.tscn" 
 
 # --- ONREADY VARIABLES ---
@@ -9,47 +9,55 @@ extends Control
 #@onready var message_label: Label = $CenterContainer/MessageLabel
 @onready var restart_btn: BaseButton = $CenterContainer/RestartButton
 @onready var menu_btn: BaseButton = $CenterContainer/MenuButton
+@onready var win_sfx = $WinSFX
+@onready var lose_sfx = $LoseSFX
 
 func _ready() -> void:
-	# Nascondiamo la scena all'inizio se per caso è visibile nell'editor
+	# Hide scene on ready
 	visible = false
 	
-	# Connessione segnali bottoni
+	# Connect button signals
 	restart_btn.pressed.connect(_on_restart_pressed)
 	menu_btn.pressed.connect(_on_menu_pressed)
 
 # --- PUBLIC FUNCTIONS ---
 
-## Chiamata dal GameManager per mostrare la vittoria
+## Call from GameManager for win
 func show_victory() -> void:
 	title_label.text = "VICTORY!"
 	#message_label.text = "Castle Destroyed!"
 	_show_screen()
+	
+	# Play SFX
+	win_sfx.play()
 
-## Chiamata dal GameManager per mostrare la sconfitta
+## Call from GameManager for lose
 func show_game_over() -> void:
 	title_label.text = "GAME OVER"
 	#message_label.text = "Out of lives..."
 	_show_screen()
+	
+	# Play SFX
+	lose_sfx.play()
 
 # --- PRIVATE FUNCTIONS ---
 
 func _show_screen() -> void:
 	visible = true
-	# Importante: Mette in pausa il gioco sotto (blocca fisica e tutto)
+	# IMPORTANT	: PAUSE GAME (stops physics and all the rest)
 	get_tree().paused = true
 	
-	# Animazione opzionale di comparsa (es. Fade In o Pop Up)
+	# Fade in animation
 	modulate.a = 0.0
 	var tween = create_tween().set_pause_mode(Tween.TWEEN_PAUSE_PROCESS) # Il tween deve funzionare anche in pausa!
 	tween.tween_property(self, "modulate:a", 1.0, 0.5)
 
 func _on_restart_pressed() -> void:
-	# Togliamo la pausa prima di ricaricare, altrimenti la nuova scena parte bloccata!
+	# Unpause
 	get_tree().paused = false
 	get_tree().reload_current_scene()
 
 func _on_menu_pressed() -> void:
 	get_tree().paused = false
-	# Usa change_scene_to_file con il path stringa
+	# Use change_scene_to_file with path string
 	get_tree().change_scene_to_file(title_screen_scene)
